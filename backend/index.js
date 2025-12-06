b7a3402592b96a34e1412eeb8208ae108b14998a
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-12-06 16:41:08 +0530

    bugfix

--- backend/index.js
+++ backend/index.js
@@ -118,19 +118,6 @@ app.post("/generate-pdf", async (req, res) => {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Bhandal Roadways Consignee Copy</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-      integrity="sha512-..."
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Playwrite+NO:wght@100..400&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-      rel="stylesheet"
-    />
     <style>
       @font-face {
         font-family: "impact";
