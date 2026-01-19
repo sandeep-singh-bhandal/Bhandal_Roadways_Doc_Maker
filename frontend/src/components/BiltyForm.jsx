03a7be7c1e2458711981a205d37085e2dfa5f20e
Sunny <155150793+sandeep-310@users.noreply.github.com>
2026-01-19 11:27:26 +0530

    frontend fix

--- frontend/src/components/BiltyForm.jsx
+++ frontend/src/components/BiltyForm.jsx
@@ -11,7 +11,7 @@ const BiltyForm = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  const rows = [...Array(51).keys()];
 
   const handlePackageSelect = (row) => {
     setNoOfPackages(row);
